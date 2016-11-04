05a56f2019a29028e5e7e433fe6911af1bacdd2a
nicolas <nickh93@outlook.com>
2016-11-04 15:24:48 -0400

    got it to work

---
+++ config/environment.js
@@ -0,0 +1,53 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'ask-an-expert',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    firebase: {
+      apiKey: 'AIzaSyCOCGTCcuFj7zJjCdJZYEeuXN_ScjrXfPw',
+      authDomain: 'ask-an-expert-c4e77.firebaseapp.com',
+      databaseURL: 'https://ask-an-expert-c4e77.firebaseio.com',
+      storageBucket: 'ask-an-expert-c4e77.appspot.com' 
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
