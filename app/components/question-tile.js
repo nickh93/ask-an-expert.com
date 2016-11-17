c53e72bd50d5fec91eae8ecb93dae50208f715d8
nicolas <nickh93@outlook.com>
2016-11-16 19:18:31 -0500

    add a computed property that displays the amount of times the question has been asnwered

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  updateQuestionForm: false
+  updateQuestionForm: false,
+
+  answerCount: Ember.computed('question.answers', function() {
+    var countOfAnswers = this.get('question.answers').get('length');
+    if (countOfAnswers === 0) {
+      return ' nobody has answered this yet';
+    }
+    else {
+      return 'this answer has been answered ' + countOfAnswers + ' times';
+    }
+  })
 });
